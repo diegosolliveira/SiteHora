875c1dfdbdae9b8ac0e5bca4d1e566f72a55c6c0
Diego Oliveira <diegolvr14@gmail.com>
2022-03-22 17:39:31 -0300

    Adicionado a pasta com os documentos necessários.

---
+++ Site Hora do dia/script.js
@@ -0,0 +1,27 @@
+function carregar(){
+    
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = 'Agora são ' + hora + ' horas.'
+    
+    if(hora >= 0 && hora < 12){
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#bc7954';
+    }
+
+    else if(hora >= 12 && hora < 18){
+        
+        img.src = 'fototarde.png'
+        document.body.style.background = '#9a6261';
+    }
+
+    else{
+
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#312943';
+    }
+}
+
+
